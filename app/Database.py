8d14b56464d569aac78f557adf6175572b2e5183
november-kilo <natasha.i.koroleva@gmail.com>
2020-07-19 20:38:27 -0500

    append or replace db

--- app/Database.py
+++ app/Database.py
@@ -5,5 +5,10 @@ from app import engine, Constants
 
 class Database:
     @staticmethod
-    def write(data_frame: pd.DataFrame):
-        data_frame.to_sql(con=engine, name=Constants.TABLENAME, if_exists='append', index=False)
+    def write(data_frame: pd.DataFrame, should_replace):
+        if should_replace:
+            if_exists = 'replace'
+        else:
+            if_exists = 'append'
+
+        data_frame.to_sql(con=engine, name=Constants.TABLENAME, if_exists=if_exists, index=False)
--- csv_to_db.py
+++ csv_to_db.py
@@ -7,6 +7,7 @@ from app.Database import Database
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-r', '--replace', action='store_true')
     arg_parser.add_argument('-s', '--source', default='./data/data.csv')
     args = arg_parser.parse_args()
 
@@ -14,6 +15,7 @@ if __name__ == '__main__':
         with open(args.source, 'r') as csv_source_file:
             data_frame = pd.read_csv(csv_source_file)
             data_frame['event_date'] = pd.to_datetime(data_frame['event_date'], format='%d %B %Y')
-            Database.write(data_frame)
+            Database.write(data_frame, args.replace)
+            print('Done.')
     else:
         print(f'File {args.source} not found.')
